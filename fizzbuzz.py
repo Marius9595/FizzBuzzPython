5886a9905fd568021a24d379bad0f740c310d28b
Mario S. Pinto Miranda <mario.pinto.miranda@gmail.com>
2022-03-29 00:08:04 +0100

    test_if_number_is_divisible_by_3_returns_fizz_word ok

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -3,7 +3,10 @@ def fizz_buzz():
     pass
 
 def evaluate_number(number):
-    return number
 
+    if number % 3 == 0:
+        return 'Fizz'
+    else:
+        return number
 
 fizz_buzz()
--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -19,7 +19,7 @@ class TestFizzBuzz(TestCase):
         self.assertEqual(number4, 77)
         self.assertEqual(number5, 97)
 
-    def test_if_number_is_divisible_by_4_returns_fizz_word(self):
+    def test_if_number_is_divisible_by_3_returns_fizz_word(self):
 
         number1 = evaluate_number(3)
         number2 = evaluate_number(30)
